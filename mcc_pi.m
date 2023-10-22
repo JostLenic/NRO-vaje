b34b8393cfd1cc0e364165d8ae0465065dfbbd9e
JostLenic <146968809+JostLenic@users.noreply.github.com>
2023-10-22 20:33:10 +0200

    Add files via upload

---
+++ mcc_pi.m
@@ -0,0 +1,25 @@
+function pribljizek = mcc_pi(st_tock)
+    x = 2*rand(st_tock, 1) - 1;
+    y = 2*rand(st_tock, 1) - 1;
+    koorn = zeros(st_tock, 2);
+    koorz = zeros(st_tock, 2);
+    seznam_r = zeros(st_tock, 1);
+    st_not_tock = 0;
+
+    for i = 1:st_tock
+        koor = [x, y];
+        radij = sqrt(koor(i, 1)^2 + koor(i, 2)^2);
+        seznam_r(i) = radij;
+        if seznam_r(i) <= 1
+            koorn(i, :) = koor(i, :);
+            st_not_tock = st_not_tock + 1;
+        else
+            koorz(i, :) = koor(i, :);
+        end    
+    end    
+    %koordinate_tock_znotraj_kroga = koorn;
+    %koordinate_tock_znotraj_kvadrata = koorz;
+    %seznam_r;
+    %st_not_tock;
+    pribljizek = 4*st_not_tock/st_tock;
+end
